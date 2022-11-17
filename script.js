3fe4950f613cd8a243816a73f10199c0ca4e32b3
Nikolaipe <nikolaipe@getacademy.no>
2022-11-17 17:18:37 +0100

    Only føkkings works
    Litt styr med datoer MTP lesbarhet og user interface når endrer, men det fungerer.

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const taskList = [
 ];
 const group = ['Huddril', 'Jakhuilynn', 'Jihn', 'Kai', 'Molly']
 const table = document.getElementById('app');
+const dateDoneBy = [];
+let reminder = '';
 
 
 updateView();
@@ -34,7 +36,12 @@ function updateView() {
     for (let i = 0; i < taskList.length; i++) {
         html += tableRow(i);
     }
-    table.innerHTML = html;
+    table.innerHTML = html + /*html*/`
+    <tr>
+        <td> <input type="text" id="addition" /></td>
+        <td> <button onclick="addTask()">Legg til</button> </td>
+    </tr>
+    `;
 }
 
 
@@ -42,7 +49,7 @@ function tableRow(i) {
     let item = taskList[i]
     const checkDone = item.done ? 'checked="checked"' : '';
     let row;
-    if(!item.edit) row = /*html*/ `
+    if (!item.edit) row = /*html*/ `
         <tr>
             <td> ${item.task} </td>
             <td> ${item.responsible} </td>
@@ -56,14 +63,21 @@ function tableRow(i) {
     else {
         let people = comeTogether();
         row = /*html*/`<tr>
-            <td> <input type="text" value="${item.task}"> </td>
+            <td> <input type="text" id="taskInput${i}" value="${item.task}"> </td>
             <td>
-                <select id="selection${i}">
+                <label for="selection${i}">Hvem skal gjøre det?</label>
+                <select name="selection${i}" id="selection${i}">
                     ${people}
                 </select> 
             </td>
+            <td>
+                <input onchange="changeDateDoneBy(${i})" id="dateInput${i}" type="date">
+            </td>
             <td>  
-                <button onclick="saveChanges(${i})">Lagre</button>
+                <button onclick="saveChanges('${i}')">Lagre</button>
+            </td>
+            <td>  
+                ${reminder}
             </td>
             </tr>
     `}
@@ -76,25 +90,53 @@ function boxCheck(i) {
     else if (!item.done) item.dateCompleted = '';
     updateView();
 }
-function editTask(i){
+function editTask(i) {
     let item = taskList[i];
     item.edit = true;
     updateView();
 }
-function comeTogether(){
+function comeTogether() {
     let groupSelection;
-    for (let i = 0; i < group.length; i++){
+    for (let i = 0; i < group.length; i++) {
         groupSelection += /*html*/`
         <option value="${i}">${group[i]}</option>
         `;
     }
     return groupSelection
 }
-function deleteTask(i){
+function deleteTask(i) {
     taskList.splice(i)
     updateView()
 }
-function saveChanges(i){
+function saveChanges(i) {
     let item = taskList[i];
-    //finn en måte å få inn selection her
+    let newTask = document.getElementById('taskInput' + i).value;
+    let selected = document.getElementById('selection' + i);
+    let member = selected.options[selected.selectedIndex].text;
+    if (!dateDoneBy[i]) {
+        reminder = 'Velg Dato!';
+        return updateView();
+    }
+    reminder = '';
+    item.task = newTask;
+    item.done = false;
+    item.responsible = member;
+    item.dateNeeded = dateDoneBy[i];
+    item.dateCompleted = '';
+    item.edit = false;
+    updateView();
+}
+function changeDateDoneBy(i) {
+    let input = document.getElementById('dateInput' + i).value;
+    let newDate = new Date(input).toDateString();
+    dateDoneBy[i] = newDate;
+    console.log(dateDoneBy);
+}
+function addTask() {
+    let additionalTask = document.getElementById('addition').value;
+    taskList.push({
+        task: additionalTask,
+        edit: true
+    })
+    updateView();
 }
